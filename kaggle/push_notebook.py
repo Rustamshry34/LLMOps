a4b1b13b17363e20293cb9cec80360a66df4d627
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2025-12-18 02:06:58 +0400

    Update push_notebook.py

--- kaggle/push_notebook.py
+++ kaggle/push_notebook.py
@@ -9,6 +9,11 @@ os.environ["KAGGLE_KEY"]      = os.getenv("KAGGLE_KEY")
 
 kaggle.api.authenticate()
 
+metadata["enable_gpu"] = True
+metadata["keywords"] = ["accelerator", "nvidia-tesla-t4"]
+metadata["is_private"] = True
+metadata["draft"] = True          # ← yeni
+
 # temp klasörüne metadata + notebook kopyala
 os.makedirs("tmp", exist_ok=True)
 shutil.copy("kaggle/notebook.ipynb",       "tmp/")

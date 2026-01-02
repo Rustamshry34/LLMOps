86fdb9df002bfb16ae7f485d743817aeb1836edc
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2026-01-03 02:25:23 +0400

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -29,7 +29,7 @@ base_model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16 if device == "cuda" else torch.float32,
     device_map="auto"
 )
-model = PeftModel.from_pretrained(base_model, MODEL_DIR).to("cpu")
+model = PeftModel.from_pretrained(base_model, MODEL_DIR).to(device)
 model.eval()
 
 # --------------------------------------------------

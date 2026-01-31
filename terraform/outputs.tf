0c30f8457c65b8116b73fdaaa9c2760521a229df
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2026-02-01 00:29:22 +0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "instance_public_ip" {
   description = "Public IP address of the deployed inference server"
-  value       = aws_instance.ats_app.public_ip
+  value       = aws_instance.llm_app.public_ip
 }
 
 output "inference_endpoint" {
   description = "FastAPI inference endpoint URL"
-  value       = "http://${aws_instance.ats_app.public_ip}:8000/generate"
+  value       = "http://${aws_instance.llm_app.public_ip}:8000/generate"
 }
 
 output "health_check_url" {
   description = "Health check URL for the inference server"
-  value       = "http://${aws_instance.ats_app.public_ip}:8000/health"
+  value       = "http://${aws_instance.llm_app.public_ip}:8000/health"
 }
 
 output "ssh_command" {
   description = "SSH command to connect to the instance"
-  value       = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.ats_app.public_ip}"
+  value       = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.llm_app.public_ip}"
 }
